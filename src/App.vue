0ea98a19eafbc235bdacb089e5ed6a329a9bdb82
AbnerYousuf <yousufabner@gmail.com>
2026-03-16 14:09:55 -0600

    got tab navigation working. there's instant visual feedback for the user as well

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <main>hello</main>
+  <main class="max-w-2xl mx-auto px-4">
+      <nav class="font-medium text-center text-teal-400 border-b border-teal-100 mb-4">
+          <ul class="flex flex-wrap -mb-px">
+            <li
+              v-for="tab in tabs"
+              :key="tab.tabName"
+              :currentTab="currentTab"
+              :tab="tab"
+              @click="currentTab = tab.tabName"
+            >
+              <TabComponent :tab="tab" :currentTab="currentTab" @click="currentTab = tab.tabName"/>
+            </li>
+          </ul>
+      </nav>
+    </main>
 </template>
 
 <script setup lang="ts">
   import { ref } from 'vue';
   import type { Tab } from '@/types';
   import type { TabNameList } from '@/types';
+  import TabComponent from '@/components/TabComponent.vue';
 
   const tabs: Tab[] = [
     { tabName: 'General', tabIcon: 'fa-solid fa-gear' },
--- src/components/TabComponent.vue
+++ src/components/TabComponent.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="tab">
-    <slot>tab stuff goes here</slot>
-  </div>
+  <button @click="$emit('click')"
+          :class="{'p-4 border-b-2': true,
+          'border-teal-300 text-teal-600': isCurrentTab,
+          'border-transparent hover:text-teal-600 hover:border-teal-300': !isCurrentTab}
+          ">
+    {{ tab.tabName }}
+  </button>
 </template>
 
 <script setup lang="ts">
     import type { Tab } from '@/types';
+import { computed } from 'vue';
 
     const props = defineProps<{
         currentTab: string;
@@ -15,4 +20,6 @@
     const emit = defineEmits<{
         click: [];
     }>();
+
+    const isCurrentTab = computed(() => props.currentTab === props.tab.tabName);
 </script>
