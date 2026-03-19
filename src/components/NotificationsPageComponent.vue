0469a00da07dac545eb93699f037d3d291835982
AbnerYousuf <yousufabner@gmail.com>
2026-03-19 12:58:04 -0600

    implemented notification settings tab

--- src/components/NotificationsPageComponent.vue
+++ src/components/NotificationsPageComponent.vue
@@ -1,7 +1,28 @@
 <template>
-  <div class="p-4">
+  <div>
     <h2 class="text-2xl font-bold mb-4 text-teal-800">Notification Settings</h2>
     <p class="mb-4 text-teal-600">Manage your notification preferences.</p>
-    <!-- this is the content for the notifications tab -->
+    <form class="space-y-4 mx-auto">
+      <div class="flex items-center gap-1">
+        <input type="checkbox" v-model="notifications.emailNotifications" />
+        <label>Email notifications</label>
+      </div>
+      <div class="flex items-center gap-1">
+        <input type="checkbox" v-model="notifications.pushNotifications" />
+        <label>Push notifications</label>
+      </div>
+      <div class="flex items-center gap-1">
+        <input type="checkbox" v-model="notifications.smsNotifications" />
+        <label>SMS Notifications</label>
+      </div>
+
+      <button type="submit" class="btn-primary">Save</button>
+    </form>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useSettings } from '@/composables/useSettings';
+
+const { notifications } = useSettings();
+</script>
